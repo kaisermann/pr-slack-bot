3d4449c62578a299b941b4fdc7c27f00adbe6c27
Christian Kaisermann <christian@kaisermann.me>
2019-07-14 14:11:41 -0300

    Add max PR counter

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Pull Request Slack Bot
 
 ## Interactions
+
+# WIP
--- src/consts.js
+++ src/consts.js
@@ -18,3 +18,7 @@ exports.QUICK_ADDITION_LIMIT = 80;
 exports.NEEDED_REVIEWS = 2;
 
 exports.PRIVATE_TEST_CHANNELS = ['GKSCG1GRX', 'GLAM8UANR'];
+
+exports.PR_CHECK_INTERVAL_SECONDS = 20;
+// github api 5000 calls/min limit * 2 calls (pull+reviews) / how many checks per minute
+exports.MAX_PRS = ~~(5000 / (2 * (60 / exports.PR_CHECK_INTERVAL_SECONDS)));
