5b696acf60b26c0bbd11738e947c7d431cc381fa
Christian Kaisermann <christian@kaisermann.me>
2019-07-15 12:27:11 -0300

    Make check_prs check all prs simultaneously

--- src/tasks/check_prs.js
+++ src/tasks/check_prs.js
@@ -7,20 +7,22 @@ module.exports = async () => {
   const channels = DB.get_channel_list();
   let pr_count = 0;
 
-  for await (const channel of channels) {
+  await channels.reduce((channel_acc, channel) => {
     const prs = DB.get_channel_prs(channel);
     pr_count += prs.length;
 
     Logger.log('=======================================');
     Logger.log(`Channel: ${channel} - ${prs.length} PRs`);
     Logger.log('');
-    for await (const pr of prs) {
+    const pr_promises = prs.map(pr => {
       Logger.log(`${pr.slug} | ${pr.ts} (${pr.hours_since_post} hours ago)`);
-      await update_pr(pr);
-    }
+      return update_pr(pr);
+    }, []);
     Logger.log('=======================================');
     Logger.log('');
-  }
+
+    return channel_acc.then(Promise.all(pr_promises));
+  }, Promise.resolve());
 
   Logger.log(`PR count: ${pr_count}/${MAX_PRS}\n`);
   Logger.log_metrics();
