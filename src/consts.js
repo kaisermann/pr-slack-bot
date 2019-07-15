2b8b67c2ecc95d0780669f367d11d3fc56c3301b
Christian Kaisermann <christian@kaisermann.me>
2019-07-15 12:34:26 -0300

    Check PR two times per minute

--- src/consts.js
+++ src/consts.js
@@ -19,6 +19,6 @@ exports.NEEDED_REVIEWS = 2;
 
 exports.PRIVATE_TEST_CHANNELS = ['GKSCG1GRX', 'GLAM8UANR'];
 
-exports.PR_CHECK_INTERVAL_SECONDS = 20;
+exports.PR_CHECK_INTERVAL_SECONDS = 30;
 // github api 5000 calls/min limit * 2 calls (pull+reviews) / how many checks per minute
 exports.MAX_PRS = ~~(5000 / (2 * (60 / exports.PR_CHECK_INTERVAL_SECONDS)));
