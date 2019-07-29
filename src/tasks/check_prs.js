100d3a3f291fc14a24e332320066a84944b26cf5
Christian Kaisermann <christian@kaisermann.me>
2019-07-29 10:36:45 -0300

    Add max prs log

--- src/tasks/check_prs.js
+++ src/tasks/check_prs.js
@@ -1,12 +1,21 @@
 const Logger = require('../api/logger.js');
 const runtime = require('../runtime.js');
+const { MAX_PRS } = require('../consts.js');
 
 module.exports = async () => {
   const { channels } = runtime;
 
+  const total_prs = channels.reduce(
+    (acc, channel) => acc + channel.prs.length,
+    0,
+  );
+
   const channel_updates = await Promise.all(
     channels.map(channel => channel.update_prs()),
   );
+
+  console.log('');
+  console.log(`Total PRs: ${total_prs}/${MAX_PRS}`);
 
   Logger.log_metrics();
   Logger.reset_metrics();
