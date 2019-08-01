17fe65dc55894dc9b90fc0307bcd907abacebdad
Christian Kaisermann <christian@kaisermann.me>
2019-08-01 18:25:39 -0300

    admasmdasida

--- src/index.js
+++ src/index.js
@@ -1,60 +1,159 @@
 require('dotenv/config');
-const cron = require('node-cron');
 
 const Slack = require('./api/slack.js');
-const Logger = require('./api/logger.js');
+const Github = require('./api/github.js');
+const low = require('lowdb');
+const FileSyncAdapter = require('lowdb/adapters/FileSync');
+const db = low(new FileSyncAdapter('data.json'));
 
-const check_prs = require('./tasks/check_prs.js');
-const check_forgotten_prs = require('./tasks/check_forgotten_prs.js');
-const update_users = require('./tasks/update_users.js');
+db.defaults({
+  messages: {},
+}).write();
 
-const runtime = require('./runtime.js');
+const channel = 'C98FX9724'; //frontendguild
 
-const cron_options = {
-  scheduled: true,
-  timezone: 'America/Sao_Paulo',
+const convert_date_to_slack_ts = date => date.getTime() / 1000;
+const convert_slack_ts_to_date = ts => new Date(parseFloat(ts, 10) * 1000);
+
+const subtract_dates = (d1, d2) => {
+  if (typeof d1 === 'string') d1 = new Date(d1);
+  if (typeof d2 === 'string') d2 = new Date(d2);
+  const diff = (d2.getTime() - d1.getTime()) / 1000 / 60;
+  return Math.abs(Math.round(diff));
 };
 
-runtime.init();
+const PR_REGEX = /github\.com\/([\w-.]*)?\/([\w-.]*?)\/pull\/(\d+)/i;
 
-// check_prs();
-async function check_loop() {
-  await check_prs();
-  // const interval = Math.max(60, Math.ceil((3600 / 2500) * runtime.prs.active.length));
-  setTimeout(check_loop, 10000);
-  // console.log(`Current interval: ${interval} seconds`);
-}
-check_loop();
+(async function() {
+  async function get_pr_messages() {
+    for await (const month of [1, 2, 3]) {
+      let list = [];
+      const oldest_ts = convert_date_to_slack_ts(new Date(2019, month - 1, 1));
+      const latest_ts = convert_date_to_slack_ts(new Date(2019, month, 1));
 
-// send forgotten prs message every work day at 14:00
-// check_forgotten_prs();
-cron.schedule('0 15 * * 1-5', check_forgotten_prs, cron_options);
-cron.schedule('0 10 * * 1-5', check_forgotten_prs, cron_options);
+      let response;
+      let cursor;
+      console.log('getting messages');
+      do {
+        response = await Slack.web_client.conversations.history({
+          cursor,
+          limit: 100,
+          oldest: oldest_ts,
+          latest: latest_ts,
+          channel,
+        });
+        list.push(
+          ...response.messages
+            .filter(message => message.text.match(PR_REGEX))
+            .map(({ text, ts }) => {
+              return {
+                match: text.match(PR_REGEX),
+                ts,
+                date: convert_slack_ts_to_date(ts),
+              };
+            }),
+        );
 
-// update user list every midnight
-// update_users();
-cron.schedule('0 0 * * 1-5', update_users, cron_options);
+        cursor = response.response_metadata.next_cursor;
+      } while (response.has_more);
+      console.log('got messages');
 
-Slack.on_pr_message(
-  // on new pr message
-  async pr_data => {
-    const { slug, channel: channel_id } = pr_data;
+      // list = list.slice(0, 1);
 
-    const channel = await runtime.get_or_create_channel(channel_id);
+      console.log('getting pr state');
+      list = await Promise.all(
+        list.map(async (item, i) => {
+          const data = (await Github.get_pr_data(
+            item.match[1],
+            item.match[2],
+            item.match[3],
+          )).data;
 
-    if (channel.has_pr(slug)) {
-      Logger.log(`Overwriting PR message: ${slug}`);
-      channel.replace_pr(pr_data.slug, pr_data);
-    } else {
-      Logger.log(`Watching ${slug}`);
-      channel.add_pr(pr_data);
+          if (!data) return false;
+
+          const { merged_at, merged, closed_at } = data;
+
+          console.log(`got ${i}/${list.length}`);
+          item.closed_at = closed_at;
+          item.merged_at = merged_at;
+          item.merged = merged;
+          return item;
+        }),
+      );
+      list = list.filter(Boolean);
+      console.log('gotpr state');
+
+      db.get('messages')
+        .set(month, list)
+        .write();
     }
+    console.log('done');
+  }
 
-    channel.update_pr(pr_data.slug);
-  },
-  // on pr message deleted
-  ({ channel: channel_id, deleted_ts }) => {
-    const channel = runtime.get_channel(channel_id);
-    channel.remove_pr_by_timestamp(deleted_ts);
-  },
-);
+  // await get_pr_messages();
+
+  const months = db
+    .get('messages')
+    .entries()
+    .value();
+  // "date": "2019-07-15T16:42:21.090Z",
+  // "closed_at": "2019-07-15T20:24:09Z",
+  // "merged_at": "2019-07-15T20:24:09Z",
+  // "merged": true
+
+  const results = months
+    .map(([month, list]) => {
+      return list.reduce(
+        (acc, pr) => {
+          const is_merged = pr.merged;
+          const is_closed = !is_merged && pr.closed_at;
+          if (is_merged) acc.merged++;
+          else if (is_closed) acc.closed++;
+          else acc.open++;
+
+          if (is_merged) {
+            acc.times_to_merge.push(subtract_dates(pr.date, pr.merged_at));
+          }
+
+          if (is_closed) {
+            acc.times_to_close.push(subtract_dates(pr.date, pr.closed_at));
+          }
+
+          return acc;
+        },
+        {
+          month: new Date(2019, month - 1, 1).toLocaleString('pt-br', {
+            year: 'numeric',
+            month: 'long',
+          }),
+          merged: 0,
+          closed: 0,
+          open: 0,
+          times_to_merge: [],
+          times_to_close: [],
+        },
+      );
+    })
+    .map(result => {
+      result.average_hours_to_merge =
+        result.times_to_merge.reduce((acc, diff) => acc + diff, 0) /
+        result.times_to_merge.length /
+        60;
+      result.average_hours_to_close =
+        result.times_to_close.reduce((acc, diff) => acc + diff, 0) /
+        result.times_to_close.length /
+        60;
+      result.quickest_to_merge_in_hours =
+        Math.min(...result.times_to_merge) / 60;
+      result.slowest_to_merge_in_hours = ~~(
+        Math.max(...result.times_to_merge) / 60
+      );
+
+      delete result.times_to_merge;
+      delete result.times_to_close;
+
+      return result;
+    });
+
+  console.log(results);
+})();
