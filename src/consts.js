c31d9cb49bce9ab1416e7b20cc54e852fdf37f8b
Christian Kaisermann <christian@kaisermann.me>
2019-10-07 15:53:06 -0300

    Remove unused emojis

--- src/consts.js
+++ src/consts.js
@@ -4,19 +4,15 @@ exports.GITHUB_FIELD_ID = 'XfCCUXUDPH';
 exports.BOT_NAME = 'Paul Robotson';
 
 exports.EMOJIS = {
-  watching: 'robot_face',
   approved: 'white_check_mark',
   ready_to_merge: 'ready-to-merge',
   commented: 'speech_balloon',
   merged: 'merged',
   changes_requested: 'changes',
-  needs_attention: 'alert',
   closed: 'closedpr',
   dirty: 'warning',
   unknown: 'shrug',
-  review_requested: 'clock3',
   waiting: 'sonic_waiting',
-  quick_read: 'zap',
   pending_review: 'eyes',
   size_small: 'pr-small',
   size_medium: 'pr-medium',
