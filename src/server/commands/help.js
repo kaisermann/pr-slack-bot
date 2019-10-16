023bf28c88b3e77406d6bcf675908866717ae61a
Christian Kaisermann <christian@kaisermann.me>
2019-10-16 12:55:37 -0300

    Refactor help command

--- src/server/commands/help.js
+++ src/server/commands/help.js
@@ -1,49 +1,54 @@
 const { EMOJIS, PR_SIZES } = require('../../consts.js');
 
-module.exports = async () => {
-  function format_section(items, title) {
-    return `*${title}*:\n${items
-      .map(([emoji, str]) => `:${emoji}: - ${str}`)
-      .join(';\n')}.`;
-  }
+const commands = [
+  ['`/pr help`', 'list all emoji meanings and commands'],
+  ['`/pr list`', 'list all open prs on the channel'],
+  ['`/pr list mine`', 'list all of your PRs on the channel'],
+  ['`/pr list @user`', 'list all PRs on the channel from a specific user'],
+  [
+    '`@Paul Robotson roulette|random`',
+    'on a PR Thread, mention a random person from the channel',
+  ],
+  [
+    '`@Paul Robotson roulette|random group-name`',
+    'on a PR Thread, mention a random person from a specific slack group',
+  ],
+];
 
-  const sizes = PR_SIZES.map(([label, max], i) => [
+const emojis = [
+  ...PR_SIZES.map(([label, max], i) => [
     EMOJIS[`size_${label}`],
     `PR of small size _(${
-      max === Infinity ? `>${PR_SIZES[i - 1][1] + 1}` : `<=${max}`
+      max !== Infinity ? `<=${max}` : `>${PR_SIZES[i - 1][1] + 1}`
     } changes)_`,
-  ]);
+  ]),
+  [EMOJIS.pending_review, 'Someone is reviewing'],
+  [EMOJIS.waiting, 'Awaiting reviews'],
+  [EMOJIS.commented, 'Someone has commented'],
+  [EMOJIS.changes_requested, 'Some changes were requested'],
 
-  const reviews = [
-    [EMOJIS.pending_review, 'Someone is reviewing'],
-    [EMOJIS.waiting, 'Awaiting reviews'],
-    [EMOJIS.commented, 'Someone has commented'],
-    [EMOJIS.changes_requested, 'Some changes were requested'],
-  ];
+  [
+    EMOJIS.dirty,
+    'The head branch is dirty and may need a rebase with the base branch',
+  ],
+  [EMOJIS.ready_to_merge, 'Ready to be merged without approvals'],
+  [EMOJIS.approved, ' Ready to be merged AND approved'],
+  [EMOJIS.merged, 'PR was merged'],
+  [EMOJIS.closed, 'PR was closed'],
+  [
+    EMOJIS.unknown,
+    'Some unknown action was taken. Please report it :robot_face:',
+  ],
+].map(([emoji, label]) => [`:${emoji}:`, label]);
 
-  const pr_states = [
-    [
-      EMOJIS.dirty,
-      'The head branch is dirty and may need a rebase with the base branch',
-    ],
-    [EMOJIS.ready_to_merge, 'Ready to be merged without approvals'],
-    [EMOJIS.approved, ' Ready to be merged AND approved'],
-    [EMOJIS.merged, 'PR was merged'],
-    [EMOJIS.closed, 'PR was closed'],
-  ];
+function format_help_section(items, title) {
+  return `*${title}*:\n${items
+    .map(([cmd, str]) => `${cmd} - ${str}`)
+    .join(';\n')}.`;
+}
 
-  const other = [
-    [
-      EMOJIS.unknown,
-      'Some unknown action was taken. Please report it :robot_face:',
-    ],
-  ];
-
-  return [
-    `Here's the meaning of each emoji:`,
-    format_section(sizes, 'Sizes'),
-    format_section(reviews, 'Review related'),
-    format_section(pr_states, 'Pull request states'),
-    format_section(other, 'Miscellaneous'),
+module.exports = () =>
+  [
+    format_help_section(commands, 'Available commands'),
+    format_help_section(emojis, 'Emojis'),
   ].join('\n\n');
-};
