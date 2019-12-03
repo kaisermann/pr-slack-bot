476cba20e59ff65ddd536f6fac3a364210f32c34
Christian Kaisermann <christian@kaisermann.me>
2019-12-03 13:47:09 -0300

    Split blocks with 3000+ characters

--- src/messages/section_pr_list.js
+++ src/messages/section_pr_list.js
@@ -1,5 +1,5 @@
 const Message = require('../includes/message.js');
-const { EMOJIS } = require('../consts.js');
+const { EMOJIS, BLOCK_MAX_LEN } = require('../consts.js');
 
 function pluralize(str, n) {
   return `${n} ${str}${n > 1 ? 's' : ''}`;
@@ -62,7 +62,7 @@ module.exports = async prs => {
     },
   );
 
-  return Object.values(sections)
+  const blocks = Object.values(sections)
     .filter(section => section.list.length)
     .map(section => ({
       ...section,
@@ -70,22 +70,44 @@ module.exports = async prs => {
         (pr_a, pr_b) => pr_b.hours_since_post - pr_a.hours_since_post,
       ),
     }))
-    .map(({ title, list }) =>
-      Message.blocks.create_markdown_section(
-        `*${title}  (${list.length})*:\n${list
-          .map(pr => {
-            const {
-              hours_since_post,
-              state: { size },
-            } = pr;
+    .flatMap(({ title, list }) => {
+      const text = `*${title}  (${list.length})*:\n${list
+        .map(pr => {
+          const {
+            hours_since_post,
+            state: { size },
+          } = pr;
 
-            return (
-              `:${EMOJIS[`size_${size.label}`]}:  ` +
-              `${link_map[pr.slug]} ` +
-              `_(${format_time(hours_since_post)})_`
-            );
-          })
-          .join('\n')}`,
-      ),
-    );
+          return (
+            `:${EMOJIS[`size_${size.label}`]}:  ` +
+            `${link_map[pr.slug]} ` +
+            `_(${format_time(hours_since_post)})_`
+          );
+        })
+        .join('\n')}`;
+
+      if (text.length < BLOCK_MAX_LEN) {
+        return Message.blocks.create_markdown_section(text);
+      }
+
+      // if block text is greater than 3000 limit, split it into multiple blocks.
+      const chunks = [];
+      let sliced_text = text;
+      do {
+        let chunk = sliced_text.slice(0, BLOCK_MAX_LEN);
+        if (chunk.length === BLOCK_MAX_LEN) {
+          // slice before the last line break
+          let cut_index = chunk.lastIndexOf('\n');
+
+          if (cut_index > -1) {
+            chunk = chunk.slice(0, cut_index);
+          }
+        }
+        chunks.push(chunk);
+        sliced_text = sliced_text.slice(chunk.length);
+      } while (sliced_text.length > 0);
+      return chunks.map(Message.blocks.create_markdown_section);
+    });
+
+  return blocks;
 };
